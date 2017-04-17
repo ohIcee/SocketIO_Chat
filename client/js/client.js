8df918a0fb1f247e4eb9d522afaf5f865396e271
Marko Plaznik <thecrazy.marko@gmail.com>
2017-04-17 14:27:18 +0200

    Added functionality
    Added a check for duplicate nicknames;
    Fixed some mobile bugs;
    Added an active users list

--- client/js/client.js
+++ client/js/client.js
@@ -3,67 +3,128 @@ $('#nickname_select_view').show();
 
 var sock = io();
 
+// Settings variables
 var show_users_typing = true;
 var dynamic_message_background_colors = true;
+var show_active_users = true;
+// Set the settings variables
 sock.on('settings', (data) => {
     show_users_typing = data.showUsersTyping;
     dynamic_message_background_colors = data.DynamicMessageBGColors;
+    show_active_users = data.showActiveUsers;
+
+    if(!show_active_users) {
+        $('#users_nav').hide();
+        $('.topNavActiveUsersButton').hide();
+    }
 });
 
+// Executes when a message is recieved
 sock.on('msg', onMessage);
+// Executes when a server message is recieved
 sock.on('servermsg', onServerMessage);
 
+// If show users typing is enabled, show who is typing
 if(show_users_typing) {
+    // Executes every x seconds - set in server.js
     sock.on('user_typing_func', (data) => {
+        // If someone is typing
         if(data.isTyping) {
+
+            // <Grammar Check>
             if(data.users.length > 1) {
                 $('#user_typing_annotation').html(data.users + ' are typing...');
             } else {
                 $('#user_typing_annotation').html(data.users + ' is typing...');
             }
+            // </Grammar Check>
+
+            // Show the text
             $('#user_typing_annotation').show();
+        // If someone is NOT typing
         } else {
+            // Hide the text
             $('#user_typing_annotation').hide();
         }
     });
 }
 
+
+if(show_active_users) {
+    // Executes when server tells users the active users
+    sock.on('active users', (data) => {
+        $('#mobile_active_users').empty();
+        $('#desktop_active_users').empty();
+        data.forEach(function(element) {
+            // MOBILE
+            $('#mobile_active_users').append($('<p id="active_user">').html(element));
+
+            // DESKTOP
+            $('#desktop_active_users').append($('<p id="active_user">').html(element));
+        }, this);
+    });
+}
+
+
+// Executes when a message is recieved
 function onMessage(msg, nick) {
+    // Remove all html tags and un-needed spaces
     msg = sanitize(msg);
+    // Style the message
     var message = '<span style="font-weight: bold">' + nick + '</span><br/><span style="color: #ffffff">' + msg + '</span>';
+    // Make the welcome message text grey
     $('#chat_log ul li:first-child').css("color", "grey");
 
+    // If dynamic message background colors is enabled, dynamically change the colors
     if(dynamic_message_background_colors) {
+        // If the previous message nickname is not the current message nickname
         if(prevMessageNick != nick) {
+            // Switch the color
             switchColor();
+            // Show the message and get the background color
             $('#messages').append($('<li>').html(message).css("background-color", getCurrentColor()));
         }           
        
+        // If the previous message nickname is the current message nickname
         if(prevMessageNick == nick) {
-            /*var temp = $('#chat_log ul li:last-child');
-            temp.prev().css("background-color", getCurrentColor());*/
+            // Show the message and get the background color
             $('#messages').append($('<li>').html(message).css("background-color", getCurrentColor()));
+            // Change the previous message color to the current color
+            // (redundancy)
             $('#chat_log ul li:last-child').prev().css("background-color", getCurrentColor());
         }
     } else {
-        $('#messages').append($('<li>').html(message));
-        $('#chat_log ul li:nth-child(even)').css("background-color", "rgba(0, 0, 0, 0.25)");
+        // If dynamic message background colors is disabled,
+        // change the color for every message
+        $('#messages').append($('<li>').html(message)); // <- Shows user the message
+        $('#chat_log ul li:nth-child(even)').css("background-color", "rgba(0, 0, 0, 0.25)"); // <- Changes the color
     }
 
+    // Keep your view at the newest recieved message
     $('body').animate({ scrollTop: $('#messages').height() }, 100);
 
+    // Set the previous message nickname - 
+    // Only affected if dynamic message background colors is enabled
     prevMessageNick = nick;
 
 }
 
 if(dynamic_message_background_colors) {
 
+    // Stores the previously recieved message nickname
     var prevMessageNick = "";
 
+    // Stores the current color
+    // It's a boolean since there are only two
+    // !
+    // TRUE == transparent;
+    // FALSE = rgba(0, 0, 0, 0.25);
+    // !
     var currentcolor = true;
+    // Switches the color
     function switchColor() {
-        // true = rgba(0, 0, 0, 0.25)
-        // false = transparent
+        // If the color is true, set it false
+        // If the color is false, set it true
         if(currentcolor) {
             currentcolor = false;
         } else {
@@ -71,7 +132,10 @@ if(dynamic_message_background_colors) {
         }
     }
 
+    // Returns the current color
     function getCurrentColor() {
+        // If it's true, return transparent;
+        // If it's false, return rgba(0, 0, 0, 0,25);
         if(currentcolor) {
             return 'transparent';
         } else {
@@ -81,34 +145,51 @@ if(dynamic_message_background_colors) {
 
 }
 
+// When a server message is recieved
 function onServerMessage(msg) {
+    // Style the message
     var message = '<span style="color: red">' + msg + '</span>';
+    // Show user the message
     $('#messages').append($('<li>').html(message));
 
+    // Keep your view at the newest recieved message
     $('body').animate({ scrollTop: $('#messages').height() }, 100);
 }
 
+// Executes if show users typing is enabled
 if(show_users_typing) {
 
+    // Stores a variable if the we already
+    // broadcasted, that we stopped/started typing
     var broadcasted_typing = false;
 
+    // Runs user typing check every x miliseconds
     setInterval(function() {
         check_if_typing();
     }, 500);
 
-
+    // Checks if the user is typing
     function check_if_typing() {
 
+        // check if the length is 0 - nothing is typed
         if(jQuery.trim( $('#message_input_bottom').val() ).length != 0) {
 
-        if(!broadcasted_typing)
-            sock.emit('user typing', true);
-        broadcasted_typing = true;
+            // If we have not broadcasted the current
+            // status, broadcast it
+            if(!broadcasted_typing)
+                // Tell the server we are typing
+                sock.emit('user typing', true);
+            // Tell the variable, we are typing
+            broadcasted_typing = true;
 
         } else {
 
+            // If we have broadcasted the current
+            // status, don't broadcast it again
             if(broadcasted_typing)
+                // Tell the server we are not typing
                 sock.emit('user typing', false);
+            // Tell the variable, we are not typing
             broadcasted_typing = false;
 
         }
@@ -117,48 +198,95 @@ if(show_users_typing) {
 
 }
 
+sock.on('nickname_check_response', (response) => {
+    if(response.avalible) {
+        // Nickname avalible
+        // Set the nickname, show the chat room
+        set_nickname(response.nickname);
+        return;
+    } else {
+        // Nickname taken
+        // Disable input for 1 second for security reasons
+        $('#server_nickname_input').attr('disabled', 'disabled');
+        setTimeout(function() {
+            $('#server_nickname_input').removeAttr('disabled');
+        }, 1000);
+        return;
+    }
+});
+
+function set_nickname(nickname) {
+
+    // Tell the server our nickname
+    sock.emit('set nickname', nickname);
+    // Show the chat room
+    $('#chat_room_view').show();
+    // Hide the nickname input
+    $('#nickname_select_view').hide();
+}
+
 // Run when any key is pressed
 $(document).keydown(function(e) {
 
 
 // If enter is pressed and message_input_bottom is focused
 if(e.which == 13 && $('#message_input_bottom').is(":focus")) {
+    // If we have nothing typed, deny message send
     if(jQuery.trim( $('#message_input_bottom').val() ).length == 0) {
         return;
     }
+    // Run the send message function
     sendMessage();
     return;
 }
 
-// If enter is pressed and server_join_input is focused
+// If enter is pressed and server_nickname_input is focused
 if(e.which == 13 && $('#server_nickname_input').is(":focus")) {
+    // If the input is valid (not null)
     if($('#server_nickname_input').is(":valid")) {
+        // Get the nickname text
         var inputVal = $('#server_nickname_input').val();
-        sock.emit('set nickname', inputVal);
-        $('#chat_room_view').show();
-        $('#nickname_select_view').hide();
-        return;
-    } else {
-        return;
+        // Remove un-needed spaces before and after the string
+        inputVal = $.trim(inputVal);
+        // Sanitize the string (removing all html tags)
+        inputVal = sanitize(inputVal);
+        // Remove all spaces
+        inputVal = inputVal.replace(/\s/g,'');
+
+        // Tell the server to check if nickname is avalible
+        sock.emit('check_nickname', inputVal);
     }
 }
 
 // If a key other than enter is pressed
 if(e.which != 13) {
+    // If we are in the chat room
     if($('#chat_room_view').is(":visible")) {
+        // Focus the message input, so
+        // we can immediately start typing
         $('#message_input_bottom').focus();
     }
+    // If we are choosing our nickname
     if($('#nickname_select_view').is(":visible")) {
+        // Focus the message input, so
+        // we can immediately start typing
         $('#server_nickname_input').focus();
     }
 }
     
 });
 
+// Send our message to the server
 function sendMessage() {
+    // Tells the server our message
     sock.emit('msg', $('#message_input_bottom').val());
+    // Empty the message input field
     $('#message_input_bottom').val('');
 }
+
+
+
+// Text sanitization code - Not commented
 
 var protos = document.body.constructor === window.HTMLBodyElement;
     validHTMLTags  =/^(?:a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|bgsound|big|blink|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|data|datalist|dd|del|details|dfn|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|img|input|ins|isindex|kbd|keygen|label|legend|li|link|listing|main|map|mark|marquee|menu|menuitem|meta|meter|nav|nobr|noframes|noscript|object|ol|optgroup|option|output|p|param|plaintext|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|spacer|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr|xmp)$/i;
@@ -189,4 +317,14 @@ function sanitize(txt) {
     tmp.innerHTML = txt;
 
     return "textContent" in tmp ? tmp.textContent : tmp.innerHTML;
+}
+
+if(show_active_users) {
+    function open_users_nav() {
+        document.getElementById("users_nav").style.width = "280px";
+    }
+
+    function close_users_nav() {
+        document.getElementById("users_nav").style.width = "0";
+    }
 }
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*
+
+// TODO:
+- More rooms
+- Improve colors
+- Language filter
+- Chat history (~ 10 messages)
+
+*/
+
 'use strict';
 
 let http = require('http');
@@ -25,14 +35,22 @@ io.on('connection', onConnection);
     // Changes color only when new user types a message
     var dynamic_message_background_colors = true;
 
+    // Show who is currently active
+    var show_active_users = true;
+
 // </>
 
+// < create arrays >
 if(show_users_typing)
     var users_typing = [];
+
+var active_users = [];
+// </ create arrays >
 
 app.use(express.static(__dirname + '/client'));
 server.listen(port, () => console.log('Server ready! Listening on port ' + port));
 
+// Log the settings into the console
 console.log('\n\nSettings : ');
 if(log_chat) { console.log('> [ENABLED] Chat logging'); }
 else { console.log('> [DISABLED] Chat logging'); }
@@ -42,57 +60,119 @@ if(dynamic_message_background_colors) { console.log('> [ENABLED] Dynamic message
 else { console.log('> [DISABLED] Dynamic message background colors'); }
 console.log('--> Change options in server.js\n\n\n');
 
+// When a new user connects
 function onConnection(sock) {
     console.log('User connected... Waiting for nickname (' + sock.id + ')');
 
-    io.emit('settings', { showUsersTyping: show_users_typing, DynamicMessageBGColors: dynamic_message_background_colors });
+    // Send settings to the user
+    io.emit('settings', { showUsersTyping: show_users_typing, DynamicMessageBGColors: dynamic_message_background_colors, showActiveUsers: show_active_users });
 
+    // Executes when a user sends a message
     sock.on('msg', (txt) => {
+        // If the user did not choose a nickname, disconnect him
         if(sock.nickname == null) {
             sock.disconnect();
         }
+        // Send the message to every other user
         io.emit('msg', txt, sock.nickname);
+        // Log the message if enabled
         if(log_chat) { console.log(sock.nickname + ' : ' + txt); }
     });
 
-    sock.on('set nickname', (nickname) => {
-        sock.nickname = nickname;
-        io.emit('servermsg', sock.nickname + ' has joined the chat.');
-        console.log(sock.id + ' -> ' + sock.nickname);
+    // Check for duplicate nicknames
+    sock.on('check_nickname', (nickname) => {
+        var found_dupe = false;
+        active_users.forEach(function(element) {
+            if(element == nickname) {
+                found_dupe = true;
+                return;
+            }
+        }, this);
+
+        // Check if a duplicate was found and tell user the response
+        if(found_dupe) {
+            io.sockets.connected[sock.id].emit('nickname_check_response', { avalible: false });
+        } else {
+            io.sockets.connected[sock.id].emit('nickname_check_response', { avalible: true, nickname: nickname });
+        }
     });
 
+    // Executes when a user sets his nickname
+    sock.on('set nickname', (nickname) => {
+        // Set the nickname
+        sock.nickname = nickname;
+        // Tell users a new user joined
+        io.emit('servermsg', sock.nickname + ' has joined the chat.');
+        // Add this user into the active users array
+        active_users.push(sock.nickname);
+        if(show_active_users) {
+            // Tell users everyone who is active
+            emit_active_users();
+        }
+    });
+
+    // If show users typing is enabled execute the following code
     if(show_users_typing) {
 
+        // When a user starts or stops typing
         sock.on('user typing', (data) => {
+            // If he started typing
             if(data) {
+                // Log who is typing
                 console.log(sock.nickname + ' is typing');
+                // Add the user to the array of currently typing users
                 users_typing.push(sock);
+                
+            // If he stopped typing
             } else {
+                // Log who stopped typing
                 console.log(sock.nickname + ' stopped typing');
+                // Run function to tell who stopped typing
                 remove_array_element(users_typing, sock);
             }
         });
 
+        // Check who is typing every x miliseconds
         setInterval(function() {
+            // If the array is empty no one is typing
             if(users_typing.length <= 0) {
+                // Tell users that no one is typing
                 io.emit('user_typing_func', false);
+
+            // If the array is not empty, someone is typing
             } else {
+                // Create an array of nicknames
                 var users_typing_nicknames = [];
+                // Retrrieve nicknames from array
                 users_typing.forEach(function(element) {
+                    // Add nicknames top array
                     users_typing_nicknames.push(element.nickname);
                 }, this);
+                // Send JSON of users typing
                 io.emit('user_typing_func', { isTyping: true, users: users_typing_nicknames});
             }
         }, 1000);
 
     }    
 
+    // Executes when a user disconnects
     sock.on('disconnect', () => {
+        // If the nickname is set, log the nickname that disconnected
         if(sock.nickname != null) { console.log(sock.nickname + ' diconnected.'); }
+        // else, log the id that disconnect
         else { console.log(sock.id + ' disconnected'); }
 
+        // Remove this user from the list
+        remove_array_element(active_users, sock.nickname);
+        // Tell users everyone who is active
+        emit_active_users();
+
+        // If the option is enabled, remove the user from the array
         if(show_users_typing) {
+            // Remove from array
             remove_array_element(users_typing, sock);
+            // If nickname is set, tell users who left the chat;
+            // Avoids "undefined"
             if(sock.nickname != null)
                 io.emit('servermsg', sock.nickname + ' left the chat.');
         }
@@ -100,6 +180,12 @@ function onConnection(sock) {
 
 }
 
+// Tell users who is currently active
+function emit_active_users() {
+    io.emit('active users', active_users);
+}
+
+// Remove an element from an array
 function remove_array_element(array, element) {
     var i = array.indexOf(element);
     if(i != -1) {
